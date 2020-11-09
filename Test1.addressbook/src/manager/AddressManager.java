5def7c4e2f6a7d6240365ef83df6ac33b7ac50be
DESKTOP-1QVGTGC\HJ <hayoungjung55@gmail.com>
2020-11-09 19:43:18 +0900

    no message

--- Test1.addressbook/src/manager/AddressManager.java
+++ Test1.addressbook/src/manager/AddressManager.java
@@ -7,7 +7,7 @@ import vo.AddressVO;
  * 주소 정보 처리 클래스. 등록, 검색, 파일 입출력 등의 기능을 수행
  */
 public class AddressManager {
-//	등록된 회원 정보를 가지고 있는 list객체 
+//	등록된 회원 정보를 가지고 있는 list객체
 	ArrayList<AddressVO> list;
 
 	/**
@@ -21,7 +21,7 @@ public class AddressManager {
 
 	/**
 	 * 전달된 새 주소를 리스트에 등록한다.
-	 * 
+	 *
 	 * @param vo 등록할 정보를 담은 객체
 	 */
 	public void addAddress(AddressVO vo) {
@@ -30,7 +30,7 @@ public class AddressManager {
 
 	/**
 	 * 전체 주소 정보 리스트를 리턴한다.
-	 * 
+	 *
 	 * @return 주소정보 객체들이 저장된 리스트
 	 */
 	public ArrayList<AddressVO> getList() {
@@ -40,7 +40,7 @@ public class AddressManager {
 
 	/**
 	 * 이름으로 주소 정보 검색
-	 * 
+	 *
 	 * @param name 검색할 이름
 	 * @return 검색된 객체 또는 없으면 null
 	 */
@@ -53,7 +53,7 @@ public class AddressManager {
 //			if (name.equals(list.get(i).getName())) {
 //				vo = list.get(i);
 //				break;
-//				
+//
 //			}
 //		}
 
@@ -69,7 +69,7 @@ public class AddressManager {
 
 	/**
 	 * 그룹으로 주소 정보 검색
-	 * 
+	 *
 	 * @param group 검색할 그룹
 	 * @return 검색된 객체들을 담은 리스트
 	 */
@@ -85,7 +85,7 @@ public class AddressManager {
 		for (AddressVO addressVO : list) {
 			if (group.equals(addressVO.getGroup())) {
 				result.add(addressVO);
-				
+
 			}
 		}
 		return result;
@@ -94,24 +94,24 @@ public class AddressManager {
 
 	/**
 	 * 이름으로 주소 정보 삭제
-	 * 
+	 *
 	 * @param name 삭제할 이름
 	 * @return 삭제 성공 여부
 	 */
 	public boolean removeAddress(String name) {
 		boolean result = false;
-		
+
 //		for (int i = 0; i < list.size(); i++) {
 ////			AddressVO vo = list.get(i);
 ////			String savedName = vo.getName();
-//			
+//
 //			if (name.equals(list.get(i).getName())) {
 //				list.remove(list.get(i));
 //				result = true;
-//				
+//
 //			}
 //		}
-		
+
 		int count = 0;
 		for (AddressVO addressVO : list) {
 			if(name.equals(addressVO.getName())) {
